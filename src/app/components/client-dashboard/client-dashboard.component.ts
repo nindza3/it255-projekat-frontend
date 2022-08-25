698d0bef686ef0f2408fc0fce5f8aa0e41abc5fa
masa.petrovic <petrovicmasa@outlook.com>
2022-08-25 22:26:52 +0200

    add basic client-dashboard

--- src/app/components/client-dashboard/client-dashboard.component.ts
+++ src/app/components/client-dashboard/client-dashboard.component.ts
@@ -1,12 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+interface IMovie {
+  id: number;
+  name: string;
+  imdbRating: string;
+  year: number;
+}
 @Component({
   selector: 'app-client-dashboard',
   templateUrl: './client-dashboard.component.html',
   styleUrls: ['./client-dashboard.component.css'],
 })
 export class ClientDashboardComponent implements OnInit {
-  constructor() {}
+  movies: IMovie[] | null;
+  constructor(private http: HttpClient) {
+    this.movies = null;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllMovies();
+    console.log(this.movies);
+  }
+
+  getAllMovies(): void {
+    let customHeaders = new HttpHeaders().set(
+      'front-auth',
+      localStorage.getItem('user_id')!.toString()
+    );
+    console.log(customHeaders);
+    this.http
+      .get<IMovie[]>('http://localhost:3000/movies', { headers: customHeaders })
+      .subscribe({
+        next: (data: IMovie[]) => {
+          console.log(data);
+          this.movies = data;
+        },
+        error: (error: any) => {
+          // this.errorMessage = error.message;
+          console.error('There was an error!', error);
+        },
+      });
+  }
 }
